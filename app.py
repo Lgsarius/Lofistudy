122e3966f10923dc2a45302cc928a63b875e42da
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-01-24 00:43:30 +0100

    neu

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, jsonify
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    music_dirs = ['lofi', 'chillbeat']
+    music_files = []
+
+    for music_dir in music_dirs:
+        dir_path = os.path.join(app.static_folder, music_dir)
+        music_files += [url_for('static', filename=music_dir + '/' + f) for f in os.listdir(dir_path) if f.endswith('.mp3')]
+
+    return render_template('index.html', music_files=music_files)
+
+@app.route('/get_songs')
+def get_songs():
+    music_dirs = ['lofi', 'chillbeat']
+    music_files = []
+
+    for music_dir in music_dirs:
+        dir_path = os.path.join(app.static_folder, music_dir)
+        music_files += [url_for('static', filename=music_dir + '/' + f) for f in os.listdir(dir_path) if f.endswith('.mp3')]
+
+    return jsonify(music_files=music_files)
 
 if __name__ == '__main__':
      app.run(debug=True, host='0.0.0.0', port=5050)
