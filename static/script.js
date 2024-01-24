49290ebe137cde9f53968b480ab742c91ffe4563
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-01-24 15:38:30 +0100

    neu

--- static/script.js
+++ static/script.js
@@ -6,7 +6,7 @@ function updateClock() {
   if (hours < 10) hours = "0" + hours;
   if (minutes < 10) minutes = "0" + minutes;
   if (seconds < 10) seconds = "0" + seconds;
-  document.getElementById('clock').textContent = hours + ":" + minutes + ":" + seconds;
+  document.getElementById('clockgroß').textContent = hours + ":" + minutes + ":" + seconds;
 }
 
 setInterval(updateClock, 1000);
