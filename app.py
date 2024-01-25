a91e6ad8412e395d319262bdb546165bd4182ee7
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-01-25 10:59:17 +0100

    3434

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 google_blueprint = make_google_blueprint(
     client_id="97309802024-m1bt3vd3dgfcs8g7k7idngrkmvlvdb8m.apps.googleusercontent.com",
     client_secret="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",
-    scope=["profile", "email"]
+    scope=["profile", "email"],
     redirect_url="/login/google/authorized"
 )
 app.register_blueprint(google_blueprint, url_prefix="/login/google")
