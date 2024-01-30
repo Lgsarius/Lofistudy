4433014359bbcc16455b537dee5aab1c1e7ad957
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-01-30 22:39:24 +0100

    sdwed

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def resetpassword():
 def home():
     music_dirs = ['lofi', 'chillbeat']
     music_files = []
-    video_files = [url_for('static', filename=f) for f in ['bg_wp.mp4', 'bg_wp2.mp4', 'bg_wp3.mp4', 'bg_wp4.mp4', 'bg_wp5.mp4']]
+    video_files = [url_for('static', filename=f) for f in ['bg_wp.mp4', 'bg_wp2.mp4', 'bg_wp3.mp4', 'bg_wp4.mp4', 'bg_wp5.mp4', 'bg_wp6.mp4', 'bg_wp7.mp4']]
     for music_dir in music_dirs:
         dir_path = os.path.join(app.static_folder, music_dir)
         music_files += [url_for('static', filename=music_dir + '/' + f) for f in os.listdir(dir_path) if f.endswith('.mp3')]
