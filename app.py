352691d1b1d8e50029c1e13f70da1f653e82447e
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-01-31 19:57:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,10 +43,14 @@ google_blueprint = make_google_blueprint(
         "https://www.googleapis.com/auth/userinfo.profile",
         "openid",
         "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/calendar.readonly" 
     ],
     redirect_url="/google/authorized"
+    offline=True
 )
+@google_blueprint.token_loader
+def get_google_token(token=None):
+    return google_blueprint.token
+
 app.register_blueprint(google_blueprint, url_prefix="/login/google")
 from flask_dance.consumer import oauth_authorized
 from flask_dance.contrib.google import google
