91258fa406ebcee71d1fafc3d2a345aa1a800f89
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-01-31 19:08:43 +0100

    neu

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ import re
 from models import User, Note
 from extensions import db, login_manager
 from flask_dance.contrib.google import make_google_blueprint, google
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://wenginlbtrauvi:bf8dbc9bd09e18f75de9ce77018b02ddfd892133cbfe750f5050db0085fd0a94@ec2-34-241-82-91.eu-west-1.compute.amazonaws.com:5432/da93lp495dnq71?sslmode=require".replace("postgres://", "postgresql://", 1)
-
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://wenginlbtrauvi:bf8dbc9bd09e18f75de9ce77018b02ddfd892133cbfe750f5050db0085fd0a94@ec2-34-241-82-91.eu-west-1.compute.amazonaws.com:5432/da93lp495dnq71?sslmode=require".replace("postgres://", "postgresql://", 1)
+    db.init_app(app)
+    return app
+app = create_app()
 google_blueprint = make_google_blueprint(
     client_id="97309802024-m1bt3vd3dgfcs8g7k7idngrkmvlvdb8m.apps.googleusercontent.com",
     client_secret="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",
@@ -207,7 +209,5 @@ def load():
     else:
         return jsonify({})
     
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5050)
+if __name__ == "__main__":
+    app.run()
