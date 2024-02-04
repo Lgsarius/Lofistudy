f5c927be004ab45f265ed06cd7af11837bb85266
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-02-04 11:55:11 +0100

    we

--- static/script.js
+++ static/script.js
@@ -690,3 +690,15 @@ document.addEventListener('click', function(e) {
   }
 });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+document.getElementById('fullscreen-btn').addEventListener('click', function() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen(); 
+    }
+  }
+});
+});
