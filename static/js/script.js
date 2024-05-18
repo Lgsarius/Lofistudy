61716e2847c5597b6131ab107e1966ddc2590b68
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-05-19 01:21:23 +0200

    Aktualisiere Big Clock-Anzeige für bessere Performance und angepasste Farbe

--- static/js/script.js
+++ static/js/script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     document.getElementById("loadingScreen").style.display = "none";
