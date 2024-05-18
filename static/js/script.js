11ff3590d096d49a4bbf8d9e500514f383b44670
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-05-19 00:47:35 +0200

    Aktualisiere CSS- und JavaScript-Dateien für Big Clock-Anzeige

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,7 @@ function updateBIGClock() {
   const minutes = String(now.getMinutes()).padStart(2, '0');
   const seconds = String(now.getSeconds()).padStart(2, '0');
   const timeString = `${hours}:${minutes}:${seconds}`;
-  document.getElementById('clock').textContent = timeString;
+  document.getElementById('bigclock').textContent = timeString;
 }
 
 updateBIGClock();
