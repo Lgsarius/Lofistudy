e1bda13f934e58956a2f232cf73a356fe5afbfef
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-07-08 19:18:45 +0200

    Aktualisiere Big Clock-Anzeige für bessere Performance und entferne leeren Textinhalt

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function() {
   setupWatchCheckbox();
   setupBigClock();
@@ -18,12 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
   setupSocialLinks();
   setupLeaderboard();
   setupAudioSourceChange();
-=======
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.getElementById("loadingScreen").style.display = "none";
-  }, 5000);
->>>>>>> fb28f435b95ec51a01792cf543ab5789a0ee0f79
 });
 
 function setupWatchCheckbox() {
@@ -47,7 +40,6 @@ function setupBigClock() {
   setInterval(updateBIGClock, 1000); // Update every second
 }
 
-<<<<<<< HEAD
 function setupLoadingScreen() {
   setTimeout(function() {
       document.getElementById("loadingScreen").style.display = "none";
@@ -58,16 +50,6 @@ function setupWallpaperSelection() {
   const container = document.querySelector('.wallaper-selection');
   const leftArrow = document.querySelector('.scroll-arrow.left');
   const rightArrow = document.querySelector('.scroll-arrow.right');
-=======
-
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  var container = document.querySelector('.wallaper-selection');
-  var leftArrow = document.querySelector('.scroll-arrow.left');
-  var rightArrow = document.querySelector('.scroll-arrow.right');
->>>>>>> fb28f435b95ec51a01792cf543ab5789a0ee0f79
 
   container.addEventListener('wheel', function(e) {
       if (e.deltaY != 0) {
