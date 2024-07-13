7e8f3bca8c95f607abe74ffa340b7f8ec345fb53
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-07-13 16:34:26 +0200

    chore: Update socketio.run parameters for debugging and hosting configuration

--- app.py
+++ app.py
@@ -522,4 +522,4 @@ if __name__ == '__main__':
         db.create_all()
         scheduler.start()
         logging.info("Scheduler started")
-        socketio.run(app, debug=True, host='0.0.0.0', port=5050)
+        socketio.run(app, debug=False, host='0.0.0.0', port=5050)
